8b5f4e36f3204b5c2dacc4f5d87539ed955bf551
KaifUlMajed <mizanshaity@gmail.com>
2018-04-22 16:31:32 +0600

    Finished working on the votingapp

---
+++ index.js
@@ -0,0 +1,234 @@
+// Module imports
+const express = require('express');
+const db = require('./public/js/dbutils.js');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+
+
+var app = express();
+app.use(express.static(path.join(__dirname, "public")));
+// View engine
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'arsenalfc haxosmash', saveUninitialized: false, resave: false }));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Voting App',
+        user: (typeof req.session.user == 'undefined') ? '' : req.session.user
+    });
+})
+app.get('/signup', (req, res) => {
+    res.render('signup', {
+        title: 'Voting App - Sign Up',
+        user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+        status: 'Unknown'
+    });
+})
+app.post('/signup', (req, res) => {
+    db.insert(req.body.email, req.body.password, (result) => {
+        if (!result) {
+            res.render('signup', {
+                title: 'Voting App - Sign Up',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                status: 'Failure'
+            });
+        }
+        else {
+            res.render('signup', {
+                title: 'Voting App - Sign Up',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                status: 'Success'
+            });
+        }
+    });
+})
+app.get('/login', (req, res) => {
+    res.render('login', {
+        title: 'Voting App',
+        user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+        status: 'Waiting for login...'
+    });
+})
+app.post('/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    db.validate(email, password, (user) => {
+        if (!user) {
+            res.render('login', {
+                title: 'Voting App - Sign In',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                status: 'Failure'
+            });
+        }
+        else {
+            req.session.user = user.email.substring(0, user.email.indexOf('@'));
+            res.render('dashboard', {
+                title: 'Voting App - Dashboard',
+                user: req.session.user
+            });
+        }
+    });
+})
+
+app.get('/dashboard', (req, res) => {
+    let user = req.session.user;
+    if (!user) {
+        res.redirect('/login');
+    }
+    else {
+        res.render('dashboard', {
+            title: 'Voting App - Dashboard',
+            user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+        })
+    }
+})
+app.get('/createpoll', (req, res) => {
+    res.render('createpoll', {
+        title: "VotingApp - Create Polls",
+        user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+        status: "Unknown"
+    })
+})
+app.post('/createpoll', (req, res) => {
+    let owner = req.session.user;
+    let question = req.body.question;
+    let options = req.body.options;
+    db.createPoll(owner, question, options, (result) => {
+        if (!result) {
+            res.render('createpoll', {
+                title: 'Voting App - Create Polls',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                status: 'Failure'
+            });
+        }
+        else {
+            res.render('createpoll', {
+                title: 'Voting App - Create Polls',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                status: 'Success'
+            });
+        }
+    });
+})
+
+app.get('/viewpolls', (req, res) => {
+    db.viewPolls((polls) => {
+        if (!polls) {
+            res.render('dashboard', {
+                title: 'Voting App - Dashboard',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                polls: []
+            })
+        }
+        else {
+            res.render('viewpolls', {
+                title: 'Voting App - View Polls',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                polls: polls
+            });
+        }
+    })
+})
+app.get('/viewmypolls', (req, res) => {
+    let owner = req.session.user;
+    db.viewMyPolls(owner, (polls) => {
+        if (!polls) {
+            res.render('dashboard', {
+                title: 'Voting App - Dashboard',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                polls: []
+            })
+        }
+        else {
+            res.render('viewpolls', {
+                title: 'Voting App - View Polls',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                polls: polls
+            });
+        }
+    })
+})
+
+
+app.get('/votepoll', (req, res) => {
+    let pollID = req.param('pollID');
+    db.findPoll(pollID, (poll) => {
+        if (!poll) {
+            res.render('dashboard', {
+                title: 'Voting App - View Polls',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user
+            })
+        }
+        else {
+            req.session.pollID = pollID;
+            res.render('votepoll', {
+                title: 'Voting App - Vote on Poll',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                status: 'Yet to vote..',
+                poll: poll
+
+            })
+        }
+    })
+})
+
+app.post('/votepoll', (req, res) => {
+    let voter = req.session.user;
+    let pollID = req.session.pollID;
+    let answer = req.body.option;
+    if (answer.indexOf('`') != -1) {
+        answer = answer.split('`').join(' ');
+    }
+    db.voteOnPoll(voter, pollID, answer, (poll, result) => {
+        if (!poll) {
+            res.render('viewpolls', {
+                title: 'Voting App - Vote on Poll',
+                user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+            })
+        }
+        else {
+            if (result) {
+                res.render('votepoll', {
+                    title: 'Voting App - Vote on Poll',
+                    user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                    status: 'Vote Registered',
+                    poll: poll
+                })
+            }
+            else {
+                res.render('votepoll', {
+                    title: 'Voting App - Vote on Poll',
+                    user: (typeof req.session.user == 'undefined') ? '' : req.session.user,
+                    status: 'You already voted on the poll',
+                    poll: poll
+                })
+            }
+        }
+    })
+})
+
+app.get('/getPollResult', (req, res) => {
+    let pollID = req.session.pollID;
+    db.findPoll(pollID, (poll) => {
+        let results = [['Option', 'Vote']];
+        Object.keys(poll.options).forEach((option) => {
+            results.push([option, poll.options[option]]);
+        })
+        res.writeHead(200, { "Content-Type": 'text/plain' });
+        res.end(JSON.stringify(results));
+    })
+})
+
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(function (err) {
+        res.redirect('/');
+    })
+})
+
+
+app.listen(8008);
---
+++ public/js/dbutils.js
@@ -0,0 +1,109 @@
+const mongojs = require('mongojs');
+var ObjectId = mongojs.ObjectId;
+const db = mongojs('votingapp',['users', 'poll']);
+
+
+function insertUser (email, password, callback){
+    let user = {'email':email, 'password':password};
+    db.users.insert(user, (err, result)=>{
+        if (err){
+            console.log(err);
+            callback(null);
+        }
+        else callback(result);
+    });
+}
+
+function validateUser (email, password, callback){
+    let user = {'email':email, 'password':password};
+    db.users.findOne(user, (err, docs)=>{
+        if (err) console.log(err);
+        else callback(docs);
+    });
+}
+
+function createPoll (owner, question, options, callback){
+    let poll = {};
+    poll.owner = owner;
+    poll.question = question;
+    poll.options = {};
+    poll.whoVoted = [];
+    options.forEach(option => {
+        poll.options[option] = 0;    
+    });
+    db.poll.insert(poll, (err, result)=>{
+        if (err){
+            console.log(err);
+            callback(null);
+        }
+        else callback(result);
+    });
+}
+
+function viewPolls(callback){
+    db.poll.find((err, docs)=>{
+        if (err){ 
+            console.log(err);
+            callback(null);
+        }
+        else{
+            callback(docs);
+        }
+    });    
+}
+function viewMyPolls(owner, callback){
+    db.poll.find({'owner': owner}, (err, docs)=>{
+        if (err){ 
+            console.log(err);
+            callback(null);
+        }
+        else{
+            callback(docs);
+        }
+    });    
+}
+
+function findPoll(id, callback){
+    db.poll.findOne({_id: ObjectId(id)}, (err, docs)=>{
+        if (err){ 
+            console.log(err);
+            callback(null);
+        }
+        else{
+            callback(docs);
+        }
+    });    
+}
+
+
+function voteOnPoll(voter, id, answer, callback){
+    db.poll.findOne({_id: ObjectId(id)}, (err, poll)=>{
+        if (poll.whoVoted.includes(voter)){
+            callback(poll, false);
+        }
+        else{
+            poll.whoVoted.push(voter);
+            poll.options[answer]++;
+            db.poll.save(poll, (err, poll)=>{
+                if (err) {
+                    console.log(err);
+                    callback(null);
+                }
+                else{
+                    callback(poll, true);
+                }
+            })
+    }    
+    })
+}
+
+//db.poll.update({_id: ObjectId("5ad88699abd50c07b84add41")}, {$inc:{'options.No':1}})
+
+
+module.exports.insert = insertUser;
+module.exports.validate = validateUser;
+module.exports.createPoll = createPoll;
+module.exports.viewPolls = viewPolls;
+module.exports.findPoll = findPoll;
+module.exports.voteOnPoll = voteOnPoll;
+module.exports.viewMyPolls = viewMyPolls;
